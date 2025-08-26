c4b45e5b2b9d3843ebab18631ec33a67e0038fc4
Estu Sandoval <estebansaul@gmail.com>
2025-08-26 20:49:38 -0300

    Reproductor de video terminado

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const listaVideos = [
-  "./Mi Corazón Encantado.mp4",
-  "./Pegasus Fantasy.mkv",
-  "./Vuela, Pega y Esquiva.mp4",
+  
 ];
+
+var pos = 0;
 
 const botones = document.querySelectorAll(".btn");
 
@@ -10,7 +10,13 @@ const video = document.getElementById("video");
 
 const barraProgreso = document.querySelector(".progreso");
 
-video.src = listaVideos[0];
+const barraVolumen = document.querySelector(".volumen");
+
+const contenedorProgreso = document.querySelector(".container_progreso");
+
+const contenedorVolumen = document.querySelector(".container_volumen");
+
+video.src = listaVideos[pos];
 
 botones[0].addEventListener("click", () => {
   if (botones[0].classList.contains("play")) {
@@ -19,18 +25,56 @@ botones[0].addEventListener("click", () => {
     botones[0].classList.replace("play", "pausa");
   } else {
     video.pause();
-    i
     botones[0].classList.replace("pausa", "play");
   }
 });
+
+botones[1].addEventListener("click",videoAnterior);
+
+botones[2].addEventListener("click",siguienteVideo);
+
+botones[3].addEventListener("click",() => {
+  video.requestFullscreen();
+})
+
+contenedorProgreso.addEventListener("click",irATiempo);
+
+contenedorVolumen.addEventListener("click",cambiaVolumen);
 
 function cambiarTamagnoProgreso(){
     let tamagno = video.currentTime  * 100 / video.duration;
     barraProgreso.style.width = tamagno + "%";
 }
 
+function irATiempo(evento){
+  let ratonX = evento.offsetX;
+  video.currentTime = video.duration * ratonX/getComputedStyle(contenedorProgreso).width.replace("px","");
+}
 
+function cambiaVolumen(evento){
+  let ratonX = evento.offsetX;
+  video.volume =  ratonX/getComputedStyle(contenedorVolumen).width.replace("px","");
+  barraVolumen.style.width = ratonX + "px";
+}
 
-botones[3].addEventListener("click",() => {
-  video.requestFullscreen();
-})
+function siguienteVideo(){
+  if(pos < listaVideos.length - 1){
+    pos++
+  }
+  else{
+    pos = 0;
+  }
+  video.src = listaVideos[pos];
+  botones[0].classList.replace("pausa","play");
+}
+
+function videoAnterior(){
+  if(pos > 0){
+    pos--
+  }
+  else{
+    pos = listaVideos.length - 1;
+  }
+  video.src = listaVideos[pos];
+  botones[0].classList.replace("pausa","play");
+}
